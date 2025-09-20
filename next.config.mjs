7bac02fe89cfcb9208b32b790fbde60af87a27fb
nelsonaddae2512-maker <nelsonaddae2512@gmail.com>
2025-09-20 07:23:00 +0000

    Updates

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  // No basePath/assetPrefix for custom domain or user page
-};
-export default nextConfig;
