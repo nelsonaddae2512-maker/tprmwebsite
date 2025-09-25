7cd5c84aca69be430921523def04cb2d90568a9c
Nelson Addae <nelsonaddae2512@gmail.com>
2025-09-25 14:37:22 -0500

    Clean install & working build

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
->>>>>>> 44edbd0 (Initial commit with working Next.js setup)
