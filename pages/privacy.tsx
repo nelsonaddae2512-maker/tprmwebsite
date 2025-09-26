8fdb1a47f57b509f31a0c6ff671015bd4ad3e983
nelsonaddae2512-maker <nelsonaddae2512@gmail.com>
2025-09-25 22:46:17 -0500

    Create privacy.tsx

---
+++ pages/privacy.tsx
@@ -0,0 +1,6 @@
+<div className="flex gap-6">
+  <a href="/pricing" className="hover:text-indigo-600">Pricing</a>
+  <a href="/trust" className="hover:text-indigo-600">Trust</a>
+  <a href="/privacy" className="hover:text-indigo-600">Privacy</a>
+  <a href="/terms" className="hover:text-indigo-600">Terms</a>
+</div>
