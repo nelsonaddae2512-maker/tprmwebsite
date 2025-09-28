40ff004e84ac2cc4a76e1bf52b488002241e4a68
Nelson Addae <nelsonaddae2512@gmail.com>
2025-09-27 20:26:22 -0500

    fix: resolve webpack build error

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-module.exports = {
-=======
 ﻿module.exports = {
->>>>>>> 980f091 (fix: ensure Tailwind CSS loads via _app.tsx, globals.css, configs)
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
