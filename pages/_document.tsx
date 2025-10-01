a52015bd9d36e553364bf40bc583f8b9f353194b
nelsonaddae2512-maker <nelsonaddae2512@gmail.com>
2025-09-30 23:04:13 -0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,15 @@
-import HeadSeo from "../components/HeadSeo";
-import Nav from "../components/Footer";
-import Footer from "../components/Footer";
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        {/* Safe global tags only (icons, preconnects, fonts, meta) */}
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body className="bg-white text-gray-800">
         <Main />
         <NextScript />
       </body>
