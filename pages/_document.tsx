d61c330b4caba52d09f9245b86f0359f0e0165a0
nelsonaddae2512-maker <nelsonaddae2512@gmail.com>
2025-10-01 21:45:49 -0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/favicon.png" sizes="16x16" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
         <meta name="theme-color" content="#4f46e5" />
       </Head>
       <body className="bg-white text-gray-900 antialiased">
