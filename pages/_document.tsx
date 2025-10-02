92570b3988a2011a71d7f0494026dc62b9a3c231
nelsonaddae2512-maker <nelsonaddae2512@gmail.com>
2025-10-01 21:58:27 -0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+
 // pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
@@ -5,6 +6,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#4F46E5" />
         <link rel="icon" href="/favicon.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/favicon.png" sizes="16x16" type="image/png" />
         <link rel="apple-touch-icon" href="/logo.png" />
